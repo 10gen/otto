13ab17f3f26e094609bf44d95b56fc4fc9cbfdfd
Tim Sedgwick <trsedgwick@gmail.com>
2020-08-10 16:30:37 -0500

    REALMC-6592: correctly output false for IsInteger for NaN

--- value.go
+++ value.go
@@ -184,6 +184,9 @@ func (value Value) IsBoolean() bool {
 
 // IsNumber will return true if value is a number (primitive).
 func (value Value) IsNumber() bool {
+	if value.IsNaN() {
+		return false
+	}
 	return value.kind == valueNumber
 }
 
